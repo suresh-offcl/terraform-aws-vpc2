b7f610fdfe0fc458c12bcfba140cf81f50258512
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-03-24 20:45:56 +0530

    add

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_eip" "nat" {
 
 # Create a NAT Gateway using the EIP
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
+  allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
   depends_on    = [aws_internet_gateway.igw]
 
