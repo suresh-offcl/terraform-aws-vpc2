341572a1a709e932971fc8c13cc8f1ae01e53cfa
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-03-24 11:29:27 +0530

    add

---
+++ main.tf
@@ -0,0 +1,175 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnet_tags,
+    {
+        Name = "${local.resource_name}-public"
+    }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidr[count.index]
+  availability_zone       = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.private_subnet_tags,
+    {
+        Name = "${local.resource_name}-private"
+    }
+  )
+}
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.database_subnet_cidr[count.index]
+  availability_zone       = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_tags,
+    {
+        Name = "${local.resource_name}-database"
+    }
+  )
+}
+
+resource "aws_eip" "eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = local.resource_name
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public[0].id 
+
+  tags = {
+    Name = "${local.resource_name}-nat"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.route_table_public_tags,
+    {
+        Name = "${local.resource_name}-route_table-public"
+    }
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.route_table_private_tags,
+    {
+        Name = "${local.resource_name}-route_table-private"
+    }
+  )
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.route_table_database_tags,
+    {
+        Name = "${local.resource_name}-route_table-database"
+    }
+  )
+}
+
+resource "aws_route" "public" {
+
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+
+}
+
+resource "aws_route" "private" {
+
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id             = aws_nat_gateway.nat.id 
+
+}
+
+resource "aws_route" "database" {
+
+  route_table_id         = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id             = aws_nat_gateway.nat.id
+
+}
+
+resource "aws_route_table_association" "public" {
+
+  count = length(var.public_subnet_cidr)
+  subnet_id      = aws_subnet.public[count.index].id 
+  route_table_id = aws_route_table.public.id 
+
+}
+
+resource "aws_route_table_association" "private" {
+
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id 
+  route_table_id = aws_route_table.private.id 
+  
+}
+
+resource "aws_route_table_association" "database" {
+
+  count = length(var.database_subnet_cidr)
+  subnet_id      = aws_subnet.database[count.index].id 
+  route_table_id = aws_route_table.database.id 
+  
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,90 @@
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "enable_dns_support" {
+  
+  default = true 
+
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "common_tags" {
+  default = {}
+}
+
+variable "vpc_tags" {
+  default = {}
+}
+
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidr" {
+
+   type = list
+   validation {
+
+     condition = length(var.public_subnet_cidr) ==2
+     error_message = "must should have 2 cidr blocks"
+
+   }
+
+}
+
+variable "public_subnet_tags" {
+  default = {}
+}
+
+variable "private_subnet_cidr" {
+   type = list
+   validation {
+
+     condition = length(var.private_subnet_cidr) ==2
+     error_message = "must should have 2 cidr blocks" 
+
+   }
+}
+
+variable "private_subnet_tags" {
+  default = {}
+}
+
+variable "database_subnet_cidr" {
+   type = list
+   validation {
+
+     condition = length(var.database_subnet_cidr) ==2
+     error_message = "must should have 2 cidr blocks"
+
+   }
+}
+
+variable "database_subnet_tags" {
+  default = {}
+}
+
+variable "route_table_public_tags" {
+  default = {}
+}
+
+variable "route_table_private_tags" {
+  default = {}
+}
+
+variable "route_table_database_tags" {
+  default = {}
+}
+
